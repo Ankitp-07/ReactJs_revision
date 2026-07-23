b7433fa2a0af01f5c8fe57f00b774792872152a0
Ankitp-07 <152526088+Ankitp-07@users.noreply.github.com>
2026-07-24 02:12:13 +0530

    Example2 taken and returning Html to render

--- 01basics/src/App.js
+++ 01basics/src/App.js
@@ -1,6 +1,8 @@
 
 
 ///app ek function , jo return karthai html ko , aaur usko render ksr dete hainn
+
+/*
 function App() {
   return (
     <h1> create project in react example </h1>
@@ -10,3 +12,21 @@ function App() {
 export default App;
 
 //js ke through html run kar pa rhe hhain
+
+*/
+
+
+
+import Example2 from './Example2'
+
+
+function App() {
+  return (
+    <>
+    <Example2/>
+    <h1> create project  react example </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01basics/src/Example2.js
@@ -0,0 +1,6 @@
+function Example2(){
+    return(
+        <h2>CreateReact -- taking example.js to better understsnding</h2>
+    )
+}
+export default Example2 
