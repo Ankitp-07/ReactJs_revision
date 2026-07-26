1dcf04bd6ff124523fe49cd26c44a914009b5478
Ankitp-07 <152526088+Ankitp-07@users.noreply.github.com>
2026-07-26 22:44:54 +0530

    .

--- 01viteReact/src/main.jsx
+++ 01viteReact/src/main.jsx
@@ -2,8 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).
+render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+//either use <strictMode> or not : na use karne se thoda light weight hojayega
