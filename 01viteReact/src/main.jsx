20d145390722172aabe97f861610703e35fa3841
Ankitp-07 <152526088+Ankitp-07@users.noreply.github.com>
2026-07-27 20:16:38 +0530

    app function created inside the main and rendered

--- 01viteReact/src/main.jsx
+++ 01viteReact/src/main.jsx
@@ -2,11 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){     //App ek function hai jo return karta hai Html element ko 
+  return (    //main me hi bana ke check karenge functionn ko 
+
+    <div>
+      <h1>
+        Custom App !
+      </h1>
+    </div>
+  )
+}
+
 createRoot(document.getElementById('root')).
 render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  //<>
+    //<App />
+   // <MyApp/>
+    MyApp()  //yisa bhi likh skte hain 
+     //</> 
 )
 
 //either use <strictMode> or not : na use karne se thoda light weight hojayega
