9051afb4b10dbb1b2f7681f78f44e8d6b1501853
Ankitp-07 <152526088+Ankitp-07@users.noreply.github.com>
2026-08-06 02:44:58 +0530

    A component/card.jsx crreated in src and return and render by App.jsx

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import Card  from './components/card'
 
 function App() {
   return (
+    <>
     <h1 className="text-3xl font-bold underline">
       Hello Tailwind
     </h1>
+     <Card/>
+     <Card/>
+
+</>
+    
   )
 }
 
